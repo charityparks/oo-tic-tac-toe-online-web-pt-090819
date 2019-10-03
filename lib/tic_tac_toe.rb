ee59445adf13e28b0098bb6a22f60a56cee27c94
Charity Parks <tuppercharity@gmail.com>
2019-10-03 20:07:37 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -68,21 +68,8 @@ class TicTacToe
     end
   end
   
-  def turn
-    puts "Please enter 1-9:"
-    user_input = gets.strip
-    index = input_to_index(user_input)
-    if valid_move?(index)
-      move(index, current_player)
-      display_board
-    else
-       puts "Invalid selection, please choose a position between 1-9."
-       
-      #user_input = gets.strip
-      #turn
-      
-    end
-  end
+  
+    
   
    
     
