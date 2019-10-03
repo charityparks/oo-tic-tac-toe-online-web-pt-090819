af0e4a04fed0d5e4ccfb800ba3e9d76902a4dbda
Charity Parks <tuppercharity@gmail.com>
2019-10-03 19:47:07 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -68,7 +68,17 @@ class TicTacToe
     end
   end
   
-  
+  def turn
+    puts "Please enter 1-9:"
+    user_input = gets.strip
+    index = input_to_index(user_input)
+    if valid_move?(index)
+      move(index, current_player)
+      display_board
+    else
+      turn
+    end
+  end
   
    
     
