b2ce107e4ce0787f17c642de2b19ecfd02d4aa6b
Charity Parks <tuppercharity@gmail.com>
2019-10-03 19:42:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -73,9 +73,13 @@ class TicTacToe
     index = input_to_index(user_input)
     if valid_move?(index)
       move (index, current_player)
+      display_board
     else 
       turn
-      display_board
+    end
+  end
+  
+   
     
     
 
